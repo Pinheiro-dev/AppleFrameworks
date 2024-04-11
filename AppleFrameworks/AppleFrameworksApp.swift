12fe6ac1f1f28418969fbded44dc79bcb27417b7
Pinheiro-dev <mathepba@gmail.com>
2024-04-10 22:17:01 -0300

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Matheus Pinheiro on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
