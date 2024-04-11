abaa8096f71e802a60ddfaa0aa4c4a3a3cec86aa
Pinheiro-dev <mathepba@gmail.com>
2024-04-10 22:57:58 -0300

    Adiciona imagens, mock, gitignore e cria grid

---
+++ AppleFrameworks/FrameworkGridView.swift
@@ -0,0 +1,54 @@
+//
+//  FrameworkGridView.swift
+//  AppleFrameworks
+//
+//  Created by Matheus Pinheiro on 10/04/24.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        LazyVGrid(columns: columns) {
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        }
+        
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
+
+struct FrameworkTitleView: View {
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 90, height: 90)
+            
+            Text(name)
+                .lineLimit(1)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
+}
